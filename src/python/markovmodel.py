d2b88797274949795623265dd2061b525e6e2a2f
RJ Nowling <rnowling@gmail.com>
2014-07-11 22:08:54 -0400

    Began implementing Markov Model for choosing items to purchase

---
+++ src/python/markovmodel.py
@@ -0,0 +1,48 @@
+from collections import defaultdict
+
+import random
+
+class MarkovModelBuilder(object):
+	def __init__(self):
+		self.states = set()
+		self.edge_weights = defaultdict(lambda: defaultdict(lambda: 0))
+
+	def add_state(self, state_label):
+		self.states.add(state_label)
+
+	def add_edge_weight(self, start_state, end_state, weight):
+		self.edge_weights[start_state][end_state] = weight
+
+	def compute_transition_probabilities(self):
+		edge_probabilities = defaultdict(lambda: defaultdict(lambda: 0))
+		for start_state in self.states:
+			weight_sum = 0.0
+			for end_state, weight in self.edge_weights[start_state].iteritems():
+				weight_sum += weight
+			for end_state, weight in self.edge_weights[start_state].iteritems():
+				edge_probabilities[start_state][end_state] = weight / weight_sum
+		return edge_probabilities
+
+	def build_msm(self):
+		edge_probabilities = self.compute_transition_probabilities()
+		return MarkovModel(states=self.states, edge_probabilities=edge_probabilities)
+
+
+class MarkovModel(object):
+	def __init__(self, states=None, edge_probabilities=None):
+		self.states = list(states)
+		self.edge_probabilities = edge_probabilities
+		self.current_state = random.choice(self.states)
+
+	def progress_state(self):
+		r = random.random()
+		cum_sum = 0.0
+		for candidate_state, prob in self.edge_probabilities[self.current_state].iteritems():
+			cum_sum += prob
+			if r < cum_sum:
+				self.current_state = candidate_state
+				return candidate_state
+		raise Exception, "Could not find next state for Markov Model!"
+
+if __name__ == "__main__":
+	pass
--- src/python/products.py
+++ src/python/products.py
@@ -1,5 +1,82 @@
 from collections import defaultdict
 
+import random
+
+from markovmodel import MarkovModelBuilder
+
+import json
+
+class ProductCategory(object):
+	def __init__(self, fields, records):
+		self.fields = fields
+		self.records = records
+
+	def normalize_field_weights(self, field_weights):
+		weight_sum = 0.0
+		for field, weight in field_weights.iteritems():
+			weight_sum += weight
+
+		for field, weight in list(field_weights.iteritems()):
+			field_weights[field] = weight / weight_sum
+
+		return field_weights
+
+	def generate_transition_parameters(self):
+		field_weights = dict()
+		field_similarity_weights = dict()
+		for field in self.fields:
+			field_weights[field] = 1.0 # random.uniform(0.0, 1.0)
+			field_similarity_weights[field] = 1.0 # random.uniform(0.0, 1.0)
+		loopback_weight = 0.9
+
+		return field_weights, field_similarity_weights, loopback_weight
+
+	def create_markov_model(self):
+		field_weights, field_similarity_weights, loopback_weight = \
+			self.generate_transition_parameters()
+		field_weights = self.normalize_field_weights(field_weights)
+
+		builder = MarkovModelBuilder()
+		for rec in self.records:
+			builder.add_state(tuple(rec.items()))
+			for other_rec in self.records:
+				weight = 0.0
+				if rec == other_rec:
+					weight = loopback_weight
+				else:
+					for field in self.fields:
+						if rec[field] == other_rec[field]:
+							weight += field_weights[field] * field_similarity_weights[field]
+						else:
+							weight += field_weights[field] * (1.0 - field_similarity_weights[field])
+					weight = (1.0 - loopback_weight) * weight
+				builder.add_edge_weight(tuple(rec.items()), tuple(other_rec.items()), weight)
+
+		return builder.build_msm()
+
+def product_category_factory(fields, records):
+	return ProductCategory(fields, records)
+
+def load_products_json():
+	category_fl = open("product_categories.json")
+	category_descriptions = json.load(category_fl)
+	category_fl.close()
+
+	products_fl = open("products.json")
+	products = json.load(products_fl)
+	products_fl.close()
+
+	products_by_category = defaultdict(list)
+	for product in products:
+		category = product["category"]
+		products_by_category[category].append(product)
+
+	product_cateogry_objects = dict()
+	for category in category_descriptions:
+		product_cateogry_objects[category["category"]] = ProductCategory(category["fields"], products_by_category[category["category"]])
+
+	return product_cateogry_objects
+
 def load_products():
 	products = defaultdict(list)
 	fl = open("products.csv")
@@ -23,3 +100,10 @@ def load_products():
 
 	return products
 
+if __name__ == "__main__":
+	product_cateogry_objects = load_products_json()
+
+	markov_model = product_cateogry_objects["dry dog food"].create_markov_model()
+
+	for i in xrange(20):
+		print markov_model.progress_state()
