e96fc84c326e264f1e8012160c970f8bf941e0bd
RJ Nowling <rnowling@gmail.com>
2014-07-28 16:21:41 -0400

    Updated prices.  Added exclusion filters.  Updated item description.  Add price histogram.

--- src/python/product_enumerator.py
+++ src/python/product_enumerator.py
@@ -1,13 +1,18 @@
+import matplotlib
+matplotlib.use("PDF")
+
+from matplotlib import pyplot as plt
+
 DOG_FOOD_FIELDS = {
-    "base_price" : 10.79,
-    "brand" : [("Wellfed", 1.0, 0.0), ("Happy Pup", 1.0, 0.0), ("Dog Days", 1.0, 0.0), ("Chef Corgie", 1.0, 0.0), ("Nature's Cornucopia", 1.0, 0.0)],
-    "meat" : [("Chicken", 1.0, 0.0), ("Pork", 1.0, 0.0), ("Beef", 1.0, 0.0), ("Lamb", 1.0, 0.0), ("Bison", 1.0, 0.0), ("Venison", 1.0, 0.0), ("Rabbit", 1.0, 0.0), ("Salmon", 1.0, 0.0), ("Vegetarian", 1.0, 0.0)],
+    "base_price" : [("base_price", 1.0, 10.79)],
+    "brand" : [("Wellfed", 1.0, 0.0), ("Happy Pup", 1.05, 0.0), ("Dog Days", 1.1, 0.0), ("Chef Corgie", 0.8, 0.0), ("Nature's Cornucopia", 0.9, 0.0)],
+    "meat" : [("Chicken", 1.0, 0.0), ("Pork", 1.0, 0.0), ("Beef", 1.1, 0.0), ("Lamb", 1.2, 0.0), ("Bison", 1.2, 0.0), ("Venison", 1.2, 0.0), ("Rabbit", 1.1, 0.0), ("Salmon", 1.2, 0.0), ("Vegetarian", 1.0, 0.0)],
     "grain" : [("Potato", 1.0, 0.0), ("Sweet Potato", 1.0, 0.0), ("Rice", 1.0, 0.0), ("Brown Rice", 1.0, 0.0), ("Soy", 1.0, 0.0), ("Barley", 1.0, 0.0)],
     "quantity" : [(4.5, 1.0, 0.0), (15.0, 2.50, 0.0), (30.0, 3.69, 0.0)],
-    "limited_ingredient" : [(True, 1.0, 0.0), (False, 1.0, 0.0)],
+    "limited_ingredient" : [(True, 1.0, 1.25), (False, 1.0, 0.0)],
     "age" : [("Puppy", 1.0, 0.0), ("Senior", 1.0, 0.0), ("Adult", 1.0, 0.0)],
     "weight_loss" : [(True, 1.0, 0.0), (False, 1.0, 0.0)],
-    "organic" : [(True, 1.0, 0.0), (False, 1.0, 0.0)]
+    "organic" : [(True, 1.0, 2.50), (False, 1.0, 0.0)]
 }
 
 def combination_generator(field_name, choices, previous=None):
@@ -30,12 +35,9 @@ def generate_dog_food(food_fields):
     field_names = food_fields.keys()
     generator = None
     for name in field_names:
-        if name == "base_price":
-            continue
         generator = combination_generator(name, food_fields[name], previous=generator)
     items = [item for item in generator]
 
-    base_price = food_fields["base_price"]
     processed_items = []
     for item in items:
         processed_item = dict()
@@ -45,17 +47,65 @@ def generate_dog_food(food_fields):
             multipliers *= multiplier
             additions += addition
             processed_item[field_name] = value
-        price = multipliers * (base_price + additions)
+        price = multipliers * additions
+
+        # filter out certain combinations
+        if processed_item["brand"] == "Chef Corgie" and processed_item["organic"]:
+            continue
+
+        if processed_item["brand"] == "Chef Corgie" and processed_item["limited_ingredient"]:
+            continue
+
+        if processed_item["brand"] == "Chef Corgie" and processed_item["meat"] == "Venison":
+            continue
+
+        if processed_item["brand"] == "Chef Corgie" and processed_item["meat"] == "Bison":
+            continue
+
+        if processed_item["brand"] == "Chef Corgie" and processed_item["meat"] == "Rabbit":
+            continue
+
+        if processed_item["brand"] == "Chef Corgie" and processed_item["meat"] == "Fish":
+            continue
+
+        if processed_item["brand"] == "Chef Corgie" and processed_item["meat"] == "Vegetarian":
+            continue
+
+        if processed_item["brand"] == "Chef Corgie" and processed_item["grain"] == "Soy":
+            continue
+
+        if processed_item["brand"] == "Chef Corgie" and processed_item["grain"] == "Brown Rice":
+            continue
+
+        if processed_item["brand"] == "Chef Corgie" and processed_item["grain"] == "Sweet Potato":
+            continue
+
+        if processed_item["brand"] == "Nature's Cornucopia" and processed_item["organic"]:
+            continue
+
         processed_item["price"] = price
         processed_item["per_unit_cost"] = processed_item["price"] / processed_item["quantity"]
-        processed_item["description"] = "%s %s & %s" % (processed_item["brand"],
-                                                       processed_item["meat"],
-                                                       processed_item["grain"])
-        print processed_item
+
+        desc = processed_item["brand"]
+        if processed_item["organic"]:
+            desc += " Organic"
+        if processed_item["limited_ingredient"]:
+            desc += " Limited Ingredient"
+        desc += " %s & %s" % (processed_item["meat"], processed_item["grain"])
+        if processed_item["age"] == "senior":
+            desc += " for Seniors"
+        else:
+            desc += " for Puppies"
+
+        yield processed_item
 
 
 if __name__ == "__main__":
-    generate_dog_food(DOG_FOOD_FIELDS)
+    items = generate_dog_food(DOG_FOOD_FIELDS)
+    price_list = [item["price"] for item in items]
+    print "Number of items:", len(price_list)
+    plt.hist(price_list)
+    plt.savefig("prices.pdf")
     
     
     
