136ef810a340a9f3b5ea78f16449180ea52a96a2
RJ Nowling <rnowling@gmail.com>
2014-09-04 23:16:45 -0400

    Added utility create functions to Pair

--- src/java/bps-data-generator/src/com/github/rnowling/bps/datagenerator/datamodels/Pair.java
+++ src/java/bps-data-generator/src/com/github/rnowling/bps/datagenerator/datamodels/Pair.java
@@ -1,5 +1,10 @@
 package com.github.rnowling.bps.datagenerator.datamodels;
 
+import java.util.List;
+import java.util.Map;
+
+import com.google.common.collect.Lists;
+
 public class Pair<A, B>
 {
 	A first;
@@ -19,5 +24,18 @@ public class Pair<A, B>
 	public B getSecond()
 	{
 		return second;
+	}
+	
+	public static <A, B> Pair<A, B> create(A first, B second)
+	{
+		return new Pair<A, B>(first, second);
+	}
+	
+	public static <A, B> List<Pair<A, B>> create(Map<A, B> map)
+	{
+		List<Pair<A, B>> list = Lists.newArrayListWithExpectedSize(map.size());
+		for(Map.Entry<A, B> entry : map.entrySet())
+			list.add(Pair.create(entry.getKey(), entry.getValue()));
+		return list;
 	}
 }
