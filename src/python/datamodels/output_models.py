c24d878ca8864aabd8860a64e5b2e7a5987048b5
RJ Nowling <rnowling@gmail.com>
2014-09-06 23:33:32 -0400

    Moved Transaction into output_models

--- src/python/datamodels/output_models.py
+++ src/python/datamodels/output_models.py
@@ -1,3 +1,5 @@
+import hashlib
+
 class Store(object):
     def __init__(self):
         self.id = None
@@ -24,3 +26,21 @@ class Customer(object):
         return "(%s, %s, %s dogs, %s cats, %s)" % \
             (self.id, self.name, self.pets["dog"], 
              self.pets["cat"], self.location)
+
+class Transaction(object):
+    def __init__(self, customer=None, trans_time=None, purchased_items=None, store=None,
+                 trans_count=None):
+        self.store = store
+        self.customer = customer
+        self.trans_time = trans_time
+        self.purchased_items = purchased_items
+        self.trans_count = trans_count
+
+    def transaction_id(self):
+        return hashlib.md5(repr(self)).hexdigest()
+
+    def __repr__(self):
+        return "(%s, %s, %s, %s)" % (self.store.id,
+                                     self.customer.id,
+                                     self.trans_time,
+                                     self.trans_count)
--- src/python/generators/transaction_generator/__init__.py
+++ src/python/generators/transaction_generator/__init__.py
@@ -3,10 +3,10 @@ from collections import defaultdict
 import math
 import random
 
-import hashlib
-
 from algorithms.markovmodel import MarkovModelBuilder
 from algorithms.samplers import RouletteWheelSampler
+
+from datamodels.output_models import Transaction
 
 class ItemCategoryMarkovModelBuilder(object):
     def __init__(self, item_category=None, customer=None):
@@ -61,26 +61,6 @@ class ItemCategoryMarkovModelBuilder(object):
                 builder.add_edge_weight(tuple(rec.items()), tuple(other_rec.items()), weight)
 
         return builder.build_msm()
-
-
-class Transaction(object):
-    def __init__(self, customer=None, trans_time=None, purchased_items=None, store=None,
-                 trans_count=None):
-        self.store = store
-        self.customer = customer
-        self.trans_time = trans_time
-        self.purchased_items = purchased_items
-        self.trans_count = trans_count
-
-    def transaction_id(self):
-        return hashlib.md5(repr(self)).hexdigest()
-
-    def __repr__(self):
-        return "(%s, %s, %s, %s)" % (self.store.id,
-                                     self.customer.id,
-                                     self.trans_time,
-                                     self.trans_count)
-
 
 class TransactionPurchasesSimulator(object):
     def __init__(self, customer_state=None, item_categories=None):
