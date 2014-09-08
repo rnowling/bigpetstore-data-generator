9a7d6a11fb1e0eb7100b030d011d4700f7f1f802
RJ Nowling <rnowling@gmail.com>
2014-09-08 17:50:20 -0400

    Refactor out transaction time / category weight calculations into their own functions.  Start adding documentation

--- src/python/generators/transaction_generator/__init__.py
+++ src/python/generators/transaction_generator/__init__.py
@@ -57,17 +57,71 @@ class CustomerTransactionParametersGenerator(object):
 
 
 class TransactionPurchasesGenerator(object):
+    """
+    The purchases are simulated using a Hidden Markov Model.
+
+    The states of the model are purchases or stop states.
+    The purchse states are composed of the
+
+        * transaction time
+        * customer inventory
+        * purchased item (not in the initial state)
+        * number of purchases made in this transaction
+
+    The initial state is the customer inventory at the start
+    of the simulation, 0 purchases, and no item purchased.
+
+    For each state, the probabilities of the state transitions
+    are computed from the exhaustion time of each item category
+    in the customer's inventory and the probabilities of each
+    product that can be purchased according to the state of the
+    Markov Models in the customer purchasing profile.
+
+    There are no cycles in the transition graph (it's a DAG) -- the
+    HMM can only keep moving to previously undiscovered states. 
+
+    Since customers can technically make an infinite number of purchases,
+    the number of possible states is infinite.  Therefore, we can't
+    enumerate all of the states like we normally would for a markov
+    model.
+
+    Since there are a finite number of products, however, the number of
+    states reachable from the current state is simply the number of
+    products plus a stop state.  This makes the problem tractable.
+
+    The weight of a particular transition is given by:
+              p(product|category) p(category)
+
+    The probability of each category is determined from when the customer's
+    inventory will be exhausted and a Poisson process.  The probability
+    of purchasing an item in a given category is given by the current state
+    of the category's markov model in the purchasing profile.
+
+    We take advantage of this structure as follows:
+        1. Compute the probability of each category
+        2. Choose a category or stop state
+        3. Choose a product to purchase by progressing the state of the
+           category's markov model
+
+   This saves us from having to enumerate all possible next states
+   """
+
     def __init__(self, purchasing_profile, trans_params):
         self.purchasing_profile = purchasing_profile
         self.trans_params = trans_params
 
-    def _choose_category(self, customer_inventory, trans_time, num_purchases):
+    def _category_weight(self, exhaustion_time, trans_time):
         trigger_time = self.trans_params.average_purchase_trigger_time
+        remaining_time = max(exhaustion_time - trans_time, 0.0)
+        lambd = 1.0 / trigger_time
+        weight = lambd * math.exp(-lambd * remaining_time)
+        return weight
+
+    def _choose_category(self, customer_inventory, trans_time, num_purchases):
         category_weights = []
+
         for category, exhaustion_time in customer_inventory.get_exhaustion_times().iteritems():
-            remaining_time = max(exhaustion_time - trans_time, 0.0)
-            lambd = 1.0 / trigger_time
-            weight = lambd * math.exp(-lambd * remaining_time)
+            weight = self._category_weight(exhaustion_time, trans_time)
             category_weights.append((category, weight))
 
         if num_purchases != 0:
@@ -108,13 +162,17 @@ class TransactionTimeSampler(object):
     def __init__(self, customer_trans_params):
         self.customer_trans_params = customer_trans_params
 
-    def _propose_transaction_time(self, customer_inventory):
+    def _category_proposed_time(self, exhaustion_time):
         trigger_time = self.customer_trans_params.average_transaction_trigger_time
+        lambd = 1.0 / trigger_time
+        time_until_transaction = random.expovariate(lambd)
+        transaction_time = max(exhaustion_time - time_until_transaction, 0.0)
+        return transaction_time
+        
+    def _propose_transaction_time(self, customer_inventory):
         proposed_transaction_times = []
-        for category, exhaustion_time in customer_inventory.get_exhaustion_times().iteritems():
-            lambd = 1.0 / trigger_time
-            time_until_transaction = random.expovariate(lambd)
-            transaction_time = max(exhaustion_time - time_until_transaction, 0.0)
+        for exhaustion_time in customer_inventory.get_exhaustion_times().values():
+            transaction_time = self._category_proposed_time(exhaustion_time)
             proposed_transaction_times.append(transaction_time)
         
         return min(proposed_transaction_times)
