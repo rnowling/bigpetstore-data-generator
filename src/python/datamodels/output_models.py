edec2186f5a83789a5d6a5dbd112c9ff716c3d46
RJ Nowling <rnowling@gmail.com>
2014-09-11 14:14:50 -0400

    Add docstrings to output models

--- src/python/datamodels/output_models.py
+++ src/python/datamodels/output_models.py
@@ -1,6 +1,14 @@
 import hashlib
 
 class Store(object):
+    """
+    Record for stores.
+
+    id -- integer
+    name -- string
+    location -- ZipcodeRecord
+    """
+
     def __init__(self):
         self.id = None
         self.name = None
@@ -10,6 +18,14 @@ class Store(object):
         return "%s,%s,%s" % (self.name, self.location.zipcode, self.location.coords)
 
 class Customer(object):
+    """
+    Record for customers.
+
+    id -- integer
+    name -- string
+    location -- ZipcodeRecord
+    """
+
     def __init__(self):
         self.id = None
         self.name = None
@@ -20,6 +36,16 @@ class Customer(object):
             (self.id, self.name, self.location.zipcode)
 
 class Transaction(object):
+    """
+    Record for transactions
+
+    store -- Store
+    customer -- Customer
+    trans_time -- transaction time in days since start of simulation. int or long
+    purchased_items -- list of products purchased
+    trans_count -- hidden transaction id
+    """
+
     def __init__(self, customer=None, trans_time=None, purchased_items=None, store=None,
                  trans_count=None):
         self.store = store
@@ -29,6 +55,11 @@ class Transaction(object):
         self.trans_count = trans_count
 
     def transaction_id(self):
+        """
+        Compute transaction id as a hash of the transaction.
+        
+        Returns a string
+        """
         return hashlib.md5(repr(self)).hexdigest()
 
     def __repr__(self):
