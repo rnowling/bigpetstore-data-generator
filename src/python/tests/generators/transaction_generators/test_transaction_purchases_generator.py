08678553fd45396c284609c41c104f23310f5260
RJ Nowling <rnowling@gmail.com>
2014-09-14 15:26:11 -0400

    Finish tests for transaction_purchases_generator module

--- src/python/tests/generators/transaction_generators/test_transaction_purchases_generator.py
+++ src/python/tests/generators/transaction_generators/test_transaction_purchases_generator.py
@@ -6,8 +6,19 @@ from algorithms.markovmodel import MarkovModelBuilder
 
 from generators.transaction_generator.transaction_purchases_generator import PurchasingProcessesBuilder
 from generators.transaction_generator.transaction_purchases_generator import PurchasingProcesses
+from generators.transaction_generator.transaction_purchases_generator import TransactionPurchasesHiddenMarkovModel
+from generators.transaction_generator.transaction_purchases_generator import TransactionPurchasesGenerator
+from generators.transaction_generator.customer_inventory import CustomerInventoryBuilder
+
+
+from generators.purchasing_profile_generator import PurchasingProfileGenerator
+from generators.transaction_generator.customer_transaction_parameters_generator import CustomerTransactionParametersGenerator
 
 from datamodels.simulation_models import PurchasingProfile
+
+from readers import load_products
+
+import simulation_parameters as sim_params
 
 class TestPurchasingProcessesBuilder(unittest.TestCase):
     def test_build(self):
@@ -68,8 +79,76 @@ class TestPurchasingProcesses(unittest.TestCase):
         self.assertRaises(KeyError, processes.simulate_purchase, "delta")
 
 class TestTransactionPurchasesHiddenMarkovModel(unittest.TestCase):
-    pass
+    def test_init(self):
+        products_fl = sim_params.PRODUCTS_FILE
+        product_categories = load_products(products_fl)
+        
+        profile_generator = PurchasingProfileGenerator(product_categories)
+        purchasing_profile = profile_generator.generate()
+        processes_builder = PurchasingProcessesBuilder()
+        processes = processes_builder.build(purchasing_profile)
+
+        params_generator = CustomerTransactionParametersGenerator()
+        trans_params = params_generator.generate()
+
+        inventory_builder = CustomerInventoryBuilder(product_categories)        
+        customer_inventory = inventory_builder.build(trans_params)
+
+        hmm = TransactionPurchasesHiddenMarkovModel(processes, trans_params,
+                                                    customer_inventory, 0.0)
+    def test_progress_state(self):
+        products_fl = sim_params.PRODUCTS_FILE
+        product_categories = load_products(products_fl)
+        
+        profile_generator = PurchasingProfileGenerator(product_categories)
+        purchasing_profile = profile_generator.generate()
+        processes_builder = PurchasingProcessesBuilder()
+        processes = processes_builder.build(purchasing_profile)
+
+        params_generator = CustomerTransactionParametersGenerator()
+        trans_params = params_generator.generate()
+
+        inventory_builder = CustomerInventoryBuilder(product_categories)        
+        customer_inventory = inventory_builder.build(trans_params)
+
+        hmm = TransactionPurchasesHiddenMarkovModel(processes, trans_params,
+                                                    customer_inventory, 0.0)
+
+        purchase = hmm.progress_state()
+        
 
 class TestTransactionPurchasesGenerator(unittest.TestCase):
-    pass
- 
+    def test_init(self):
+        products_fl = sim_params.PRODUCTS_FILE
+        product_categories = load_products(products_fl)
+        
+        profile_generator = PurchasingProfileGenerator(product_categories)
+        profile = profile_generator.generate()
+
+        params_generator = CustomerTransactionParametersGenerator()
+        trans_params = params_generator.generate()
+
+        generator = TransactionPurchasesGenerator(profile, trans_params)
+
+    def test_simulate(self):
+        products_fl = sim_params.PRODUCTS_FILE
+        product_categories = load_products(products_fl)
+        
+        profile_generator = PurchasingProfileGenerator(product_categories)
+        profile = profile_generator.generate()
+
+        params_generator = CustomerTransactionParametersGenerator()
+        trans_params = params_generator.generate()
+
+
+        inventory_builder = CustomerInventoryBuilder(product_categories)        
+        customer_inventory = inventory_builder.build(trans_params)
+
+        generator = TransactionPurchasesGenerator(profile, trans_params)
+        trans_time = 100.0
+        purchases = generator.simulate(customer_inventory, 100.0)
+
+        self.assertIsInstance(purchases, list)
+
+        # TODO test cases with only 1 pet species
+        
