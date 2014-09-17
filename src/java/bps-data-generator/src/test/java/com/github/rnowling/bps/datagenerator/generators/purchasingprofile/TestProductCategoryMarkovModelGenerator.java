f24af833d623e308ff47efc3d8c3519e2aeee4ef
RJ Nowling <rnowling@gmail.com>
2014-09-17 16:26:24 -0400

    Added ProductCategoryMarkovModelGenerator and test

---
+++ src/java/bps-data-generator/src/test/java/com/github/rnowling/bps/datagenerator/generators/purchasingprofile/TestProductCategoryMarkovModelGenerator.java
@@ -0,0 +1,42 @@
+package com.github.rnowling.bps.datagenerator.generators.purchasingprofile;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import com.github.rnowling.bps.datagenerator.Constants;
+import com.github.rnowling.bps.datagenerator.SeedFactory;
+import com.github.rnowling.bps.datagenerator.algorithms.markovmodels.MarkovModel;
+import com.github.rnowling.bps.datagenerator.datamodels.inputs.ProductCategory;
+import com.github.rnowling.bps.datagenerator.datamodels.simulation.Product;
+import com.github.rnowling.bps.datagenerator.datareaders.ProductsReader;
+
+public class TestProductCategoryMarkovModelGenerator
+{
+
+	@Test
+	public void testGenerate() throws Exception
+	{
+		SeedFactory seedFactory = new SeedFactory(1245);
+		
+		ProductsReader reader = new ProductsReader(Constants.PRODUCTS_FILE);
+		
+		List<ProductCategory> productCategories = reader.readData();
+		
+		ProductCategory productCategory = productCategories.get(0);
+		
+		ProductCategoryMarkovModelGenerator generator = new ProductCategoryMarkovModelGenerator(productCategory, seedFactory);
+		
+		MarkovModel<Product> model = generator.generate();
+		
+		assertNotNull(model);
+		assertNotNull(model.getStartWeights());
+		assertNotNull(model.getTransitionWeights());
+		assertTrue(model.getStartWeights().size() > 0);
+		assertTrue(model.getTransitionWeights().size() > 0);
+	}
+
+}
