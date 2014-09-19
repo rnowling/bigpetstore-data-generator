5b8e1345c3620e63514bbbde777a9ee0737220bc
RJ Nowling <rnowling@gmail.com>
2014-09-19 17:48:16 -0400

    Changed constants representation for trigger times

--- src/java/bps-data-generator/src/main/java/com/github/rnowling/bps/datagenerator/Constants.java
+++ src/java/bps-data-generator/src/main/java/com/github/rnowling/bps/datagenerator/Constants.java
@@ -20,13 +20,12 @@ public class Constants
 	public static final int MIN_PETS = 1;
 	public static final int MAX_PETS = 10;
 	
-	public static final double TRANSACTION_TRIGGER_TIME_AVERAGE = 5.0; // days
-	public static final double TRANSACTION_TRIGGER_TIME_VARIANCE = 2.0;
+	public static final List<Pair<Double, Double>> TRANSACTION_TRIGGER_TIME_GAUSSIANS = ImmutableList.of(Pair.create(5.0, 2.0));
+	public static final List<Pair<Double, Double>> PURCHASE_TRIGGER_TIME_GAUSSIANS = ImmutableList.of(Pair.create(10.0, 4.0));
+	
 	public static final double TRANSACTION_TRIGGER_TIME_MAX = 10.0;
 	public static final double TRANSACTION_TRIGGER_TIME_MIN = 1.0;
 	
-	public static final double PURCHASE_TRIGGER_TIME_AVERAGE = 10.0; // days
-	public static final double PURCHASE_TRIGGER_TIME_VARIANCE = 4.0;
 	public static final double PURCHASE_TRIGGER_TIME_MAX = 20.0;
 	public static final double PURCHASE_TRIGGER_TIME_MIN = 1.0;
 	
