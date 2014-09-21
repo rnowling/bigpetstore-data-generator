b0c452e9823196772e8111b20cbc4446a64047fb
RJ Nowling <rnowling@gmail.com>
2014-09-21 17:54:32 -0400

    Add exponential sampler

---
+++ src/java/bps-data-generator/src/main/java/com/github/rnowling/bps/datagenerator/algorithms/samplers/ExponentialSampler.java
@@ -0,0 +1,22 @@
+package com.github.rnowling.bps.datagenerator.algorithms.samplers;
+
+import java.util.Random;
+
+import com.github.rnowling.bps.datagenerator.SeedFactory;
+
+public class ExponentialSampler implements Sampler<Double>
+{
+	final private Random rng;
+	final private double lambda;
+	
+	public ExponentialSampler(double lambda, SeedFactory seedFactory)
+	{
+		rng = new Random(seedFactory.getNextSeed());
+		this.lambda = lambda;
+	}
+	
+	public Double sample()
+	{
+		return - Math.log(1.0 - rng.nextDouble()) / lambda;
+	}
+}
---
+++ src/java/bps-data-generator/src/test/java/com/github/rnowling/bps/datagenerator/algorithms/samplers/TestExponentialSampler.java
@@ -0,0 +1,25 @@
+package com.github.rnowling.bps.datagenerator.algorithms.samplers;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import com.github.rnowling.bps.datagenerator.SeedFactory;
+
+public class TestExponentialSampler
+{
+
+	@Test
+	public void testSample()
+	{
+		double lambda = 1.0 / 2.0;
+		
+		SeedFactory seedFactory = new SeedFactory(1234);
+		
+		Sampler<Double> sampler = new ExponentialSampler(lambda, seedFactory);
+		
+		Double result = sampler.sample();
+		
+		assertTrue(result >= 0.0);
+	}
+}
