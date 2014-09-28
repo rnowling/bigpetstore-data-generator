c53d7bda21fc617f35677a27b19b39dad236d176
RJ Nowling <rnowling@gmail.com>
2014-09-28 15:09:22 -0400

    Add product usage trajectory and tests

---
+++ src/java/bps-data-generator/src/main/java/com/github/rnowling/bps/datagenerator/generators/transaction/ProductCategoryUsageTrajectory.java
@@ -0,0 +1,58 @@
+package com.github.rnowling.bps.datagenerator.generators.transaction;
+
+import java.util.List;
+
+import com.github.rnowling.bps.datagenerator.datamodels.Pair;
+import com.google.common.collect.Lists;
+
+public class ProductCategoryUsageTrajectory
+{
+	final private List<Pair<Double, Double>> trajectory;
+	
+	public ProductCategoryUsageTrajectory(double initialTime, double initialAmount)
+	{
+		trajectory = Lists.newArrayList();
+		this.append(initialTime, initialAmount);
+	}
+	
+	public void append(double time, double amount)
+	{
+		trajectory.add(Pair.create(time, amount));
+	}
+	
+	public double getLastAmount()
+	{
+		return trajectory.get(trajectory.size() - 1).getSecond();
+	}
+	
+	public double getLastTime()
+	{
+		return trajectory.get(trajectory.size() - 1).getFirst();
+	}
+	
+	public double amountAtTime(double time)
+	{
+		Pair<Double, Double> previous = null;
+		for(Pair<Double, Double> entry : trajectory)
+		{
+			if(entry.getFirst() > time)
+				break;
+			previous = entry;
+		}
+		
+		if(previous == null)
+			return 0.0;
+		
+		return previous.getSecond();
+	}
+	
+	public Pair<Double, Double> getStep(int idx)
+	{
+		return trajectory.get(idx);
+	}
+	
+	public int size()
+	{
+		return trajectory.size();
+	}
+}
