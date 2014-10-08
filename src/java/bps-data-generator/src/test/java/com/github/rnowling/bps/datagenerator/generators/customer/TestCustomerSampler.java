cf7a2cee70206113771080bf28d2714407703cb7
RJ Nowling <rnowling@gmail.com>
2014-10-08 15:58:38 -0400

    Add customer sampler test that I forgot to add before.

---
+++ src/java/bps-data-generator/src/test/java/com/github/rnowling/bps/datagenerator/generators/customer/TestCustomerSampler.java
@@ -0,0 +1,65 @@
+package com.github.rnowling.bps.datagenerator.generators.customer;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.github.rnowling.bps.datagenerator.Constants;
+import com.github.rnowling.bps.datagenerator.SeedFactory;
+import com.github.rnowling.bps.datagenerator.algorithms.samplers.Sampler;
+import com.github.rnowling.bps.datagenerator.datamodels.inputs.InputData;
+import com.github.rnowling.bps.datagenerator.datamodels.inputs.Names;
+import com.github.rnowling.bps.datagenerator.datamodels.inputs.ZipcodeRecord;
+import com.github.rnowling.bps.datagenerator.datamodels.outputs.Customer;
+import com.github.rnowling.bps.datagenerator.datamodels.outputs.Store;
+import com.github.rnowling.bps.datagenerator.datareaders.NameReader;
+import com.github.rnowling.bps.datagenerator.datareaders.ZipcodeReader;
+import com.github.rnowling.bps.datagenerator.generators.store.StoreSamplerBuilder;
+
+public class TestCustomerSampler
+{
+
+	@Test
+	public void testBuild() throws Exception
+	{
+		ZipcodeReader zipcodeReader = new ZipcodeReader();
+		zipcodeReader.setCoordinatesFile(Constants.COORDINATES_FILE);
+		zipcodeReader.setIncomesFile(Constants.INCOMES_FILE);
+		zipcodeReader.setPopulationFile(Constants.POPULATION_FILE);
+		List<ZipcodeRecord> zipcodes = zipcodeReader.readData();
+		
+		NameReader nameReader = new NameReader(Constants.NAMEDB_FILE);
+		Names names = nameReader.readData();
+		
+		InputData inputData = new InputData(zipcodes, names);
+		
+		SeedFactory factory = new SeedFactory(1234);
+		
+		StoreSamplerBuilder storeSamplerBuilder = new StoreSamplerBuilder(zipcodes, factory);
+		Sampler<Store> storeSampler = storeSamplerBuilder.build();
+		
+		List<Store> stores = new ArrayList<Store>();
+		for(int i = 0; i < 10; i++)
+		{
+			Store store = storeSampler.sample();
+			stores.add(store);
+		}
+		
+		CustomerSamplerBuilder builder = new CustomerSamplerBuilder(stores, inputData, factory);
+		Sampler<Customer> sampler = builder.build();
+		Customer customer = sampler.sample();
+		
+		assertNotNull(customer);
+		assertTrue(customer.getId() >= 0);
+		assertNotNull(customer.getName());
+		assertNotNull(customer.getName().getFirst());
+		assertNotNull(customer.getName().getSecond());
+		assertNotNull(customer.getLocation());
+		
+	}
+
+}
