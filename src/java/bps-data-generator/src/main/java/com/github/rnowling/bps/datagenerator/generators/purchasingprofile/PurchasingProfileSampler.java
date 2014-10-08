d84a0229fcd8a3f5633109a25b5720c597abedea
RJ Nowling <rnowling@gmail.com>
2014-10-08 15:42:08 -0400

    Turn PurchasingProfileGenerator into PurchasingProfileSampler as part of effort for increased composability

--- src/java/bps-data-generator/src/main/java/com/github/rnowling/bps/datagenerator/generators/purchasingprofile/PurchasingProfileSampler.java
+++ src/java/bps-data-generator/src/main/java/com/github/rnowling/bps/datagenerator/generators/purchasingprofile/PurchasingProfileSampler.java
@@ -4,24 +4,24 @@ import java.util.List;
 
 import com.github.rnowling.bps.datagenerator.SeedFactory;
 import com.github.rnowling.bps.datagenerator.algorithms.markovmodels.MarkovModel;
+import com.github.rnowling.bps.datagenerator.algorithms.samplers.Sampler;
 import com.github.rnowling.bps.datagenerator.datamodels.inputs.ProductCategory;
 import com.github.rnowling.bps.datagenerator.datamodels.simulation.Product;
 import com.github.rnowling.bps.datagenerator.datamodels.simulation.PurchasingProfile;
-import com.github.rnowling.bps.datagenerator.generators.Generator;
 import com.google.common.collect.ImmutableList;
 
-public class PurchasingProfileGenerator implements Generator<PurchasingProfile>
+public class PurchasingProfileSampler implements Sampler<PurchasingProfile>
 {
 	ImmutableList<ProductCategory> productCategories;
 	SeedFactory seedFactory;
 	
-	public PurchasingProfileGenerator(List<ProductCategory> productCategories, SeedFactory seedFactory)
+	public PurchasingProfileSampler(List<ProductCategory> productCategories, SeedFactory seedFactory)
 	{
 		this.productCategories = ImmutableList.copyOf(productCategories);
 		this.seedFactory = seedFactory;
 	}
 	
-	public PurchasingProfile generate() throws Exception
+	public PurchasingProfile sample() throws Exception
 	{
 		PurchasingProfileBuilder builder = new PurchasingProfileBuilder();
 		for(ProductCategory productCategory : productCategories)
--- src/java/bps-data-generator/src/test/java/com/github/rnowling/bps/datagenerator/generators/purchasingprofile/TestPurchasingProfileSampler.java
+++ src/java/bps-data-generator/src/test/java/com/github/rnowling/bps/datagenerator/generators/purchasingprofile/TestPurchasingProfileSampler.java
@@ -13,11 +13,11 @@ import com.github.rnowling.bps.datagenerator.datamodels.inputs.ProductCategory;
 import com.github.rnowling.bps.datagenerator.datamodels.simulation.PurchasingProfile;
 import com.github.rnowling.bps.datagenerator.datareaders.ProductsReader;
 
-public class TestPurchasingProfileGenerator
+public class TestPurchasingProfileSampler
 {
 
 	@Test
-	public void testGenerate() throws Exception
+	public void testSample() throws Exception
 	{
 		SeedFactory seedFactory = new SeedFactory(1245);
 		
@@ -25,9 +25,9 @@ public class TestPurchasingProfileGenerator
 		
 		List<ProductCategory> productCategories = reader.readData();
 		
-		PurchasingProfileGenerator generator = new PurchasingProfileGenerator(productCategories, seedFactory);
+		PurchasingProfileSampler generator = new PurchasingProfileSampler(productCategories, seedFactory);
 		
-		PurchasingProfile profile = generator.generate();
+		PurchasingProfile profile = generator.sample();
 		
 		assertNotNull(profile);
 		assertNotNull(profile.getProductCategories());
