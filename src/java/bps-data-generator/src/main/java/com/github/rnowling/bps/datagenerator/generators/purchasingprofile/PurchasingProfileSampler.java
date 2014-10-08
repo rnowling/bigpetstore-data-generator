6a5519b3c489a0569c0d743493e5a5c1718d9f2a
RJ Nowling <rnowling@gmail.com>
2014-10-08 15:58:17 -0400

    Refactor PurchasingProfileSampler into sampler and builder

--- src/java/bps-data-generator/src/main/java/com/github/rnowling/bps/datagenerator/generators/purchasingprofile/PurchasingProfileSampler.java
+++ src/java/bps-data-generator/src/main/java/com/github/rnowling/bps/datagenerator/generators/purchasingprofile/PurchasingProfileSampler.java
@@ -1,34 +1,29 @@
 package com.github.rnowling.bps.datagenerator.generators.purchasingprofile;
 
-import java.util.List;
+import java.util.Map;
 
-import com.github.rnowling.bps.datagenerator.SeedFactory;
 import com.github.rnowling.bps.datagenerator.algorithms.markovmodels.MarkovModel;
 import com.github.rnowling.bps.datagenerator.algorithms.samplers.Sampler;
 import com.github.rnowling.bps.datagenerator.datamodels.inputs.ProductCategory;
 import com.github.rnowling.bps.datagenerator.datamodels.simulation.Product;
 import com.github.rnowling.bps.datagenerator.datamodels.simulation.PurchasingProfile;
-import com.google.common.collect.ImmutableList;
 
 public class PurchasingProfileSampler implements Sampler<PurchasingProfile>
 {
-	ImmutableList<ProductCategory> productCategories;
-	SeedFactory seedFactory;
+	final Map<ProductCategory, Sampler<MarkovModel<Product>>> categorySamplers;
 	
-	public PurchasingProfileSampler(List<ProductCategory> productCategories, SeedFactory seedFactory)
+	public PurchasingProfileSampler(Map<ProductCategory, Sampler<MarkovModel<Product>>> categorySamplers)
 	{
-		this.productCategories = ImmutableList.copyOf(productCategories);
-		this.seedFactory = seedFactory;
+		this.categorySamplers = categorySamplers;
 	}
 	
 	public PurchasingProfile sample() throws Exception
 	{
 		PurchasingProfileBuilder builder = new PurchasingProfileBuilder();
-		for(ProductCategory productCategory : productCategories)
+		for(ProductCategory productCategory : categorySamplers.keySet())
 		{
-			ProductCategoryMarkovModelSampler generator = new ProductCategoryMarkovModelSampler(productCategory, seedFactory);
-			MarkovModel<Product> markovModel = generator.sample();
-			builder.addProfile(productCategory.getCategoryLabel(), markovModel);
+			Sampler<MarkovModel<Product>> sampler = categorySamplers.get(productCategory);
+			builder.addProfile(productCategory.getCategoryLabel(), sampler.sample());
 		}
 		
 		return builder.build();
--- src/java/bps-data-generator/src/test/java/com/github/rnowling/bps/datagenerator/generators/purchasingprofile/TestPurchasingProfileSamplerBuilder.java
+++ src/java/bps-data-generator/src/test/java/com/github/rnowling/bps/datagenerator/generators/purchasingprofile/TestPurchasingProfileSamplerBuilder.java
@@ -14,11 +14,11 @@ import com.github.rnowling.bps.datagenerator.datamodels.inputs.ProductCategory;
 import com.github.rnowling.bps.datagenerator.datamodels.simulation.PurchasingProfile;
 import com.github.rnowling.bps.datagenerator.datareaders.ProductsReader;
 
-public class TestPurchasingProfileSampler
+public class TestPurchasingProfileSamplerBuilder
 {
 
 	@Test
-	public void testSample() throws Exception
+	public void testBuild() throws Exception
 	{
 		SeedFactory seedFactory = new SeedFactory(1245);
 		
