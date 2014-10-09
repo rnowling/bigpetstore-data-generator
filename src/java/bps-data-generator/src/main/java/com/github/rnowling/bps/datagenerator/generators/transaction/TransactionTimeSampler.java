d8727baf6c4387ac81a937ba251b8b37b862cc49
RJ Nowling <rnowling@gmail.com>
2014-10-09 15:11:01 -0400

    Add time sampler

---
+++ src/java/bps-data-generator/src/main/java/com/github/rnowling/bps/datagenerator/generators/transaction/TransactionTimeSampler.java
@@ -0,0 +1,74 @@
+package com.github.rnowling.bps.datagenerator.generators.transaction;
+
+import java.util.Random;
+
+import com.github.rnowling.bps.datagenerator.SeedFactory;
+import com.github.rnowling.bps.datagenerator.algorithms.samplers.ExponentialSampler;
+import com.github.rnowling.bps.datagenerator.algorithms.samplers.Sampler;
+
+
+public class TransactionTimeSampler implements Sampler<Double>
+{
+	private final ExponentialSampler transactionTimeDiffSampler;
+	private final Random rng;
+	private final CustomerInventory customerInventory;
+	private double lastTransactionTime;
+	
+	public TransactionTimeSampler(double averageTransactionTriggerTime, 
+			CustomerInventory customerInventory,
+			SeedFactory seedFactory)
+	{
+		double lambda = 1.0 / averageTransactionTriggerTime;
+		this.transactionTimeDiffSampler = new ExponentialSampler(lambda, seedFactory);
+		rng = new Random(seedFactory.getNextSeed());
+		
+		this.customerInventory = customerInventory;
+		this.lastTransactionTime = 0.0;
+	}
+	
+	protected double categoryProposedTime(double exhaustionTime)
+	{
+		return Math.max(exhaustionTime - transactionTimeDiffSampler.sample(), 0.0);
+	}
+	
+	protected double proposeTransactionTime()
+	{
+		double minProposedTime = Double.MAX_VALUE;
+		for(Double exhaustionTime : this.customerInventory.getExhaustionTimes().values())
+		{
+			double proposedTime = this.categoryProposedTime(exhaustionTime);
+			minProposedTime = Math.min(proposedTime, minProposedTime);
+		}
+		
+		return minProposedTime;
+	}
+	
+	protected double proposedTimeProbability(double proposedTime)
+	{
+		if(proposedTime >= this.lastTransactionTime)
+		{
+			return 1.0;
+		}
+		else
+		{
+			return 0.0;
+		}
+	}
+	
+	public Double sample()
+	{
+		while(true)
+		{
+			double proposedTime = this.proposeTransactionTime();
+			double probability = this.proposedTimeProbability(proposedTime);
+			double r = rng.nextDouble();
+			
+			if(r < probability)
+			{
+				this.lastTransactionTime = proposedTime;
+				return proposedTime;
+			}
+		}
+	}
+	
+}
---
+++ src/java/bps-data-generator/src/main/java/com/github/rnowling/bps/datagenerator/generators/transaction/TransactionTimeSamplerBuilder.java
@@ -0,0 +1,31 @@
+package com.github.rnowling.bps.datagenerator.generators.transaction;
+
+import com.github.rnowling.bps.datagenerator.SeedFactory;
+
+public class TransactionTimeSamplerBuilder
+{
+	private final SeedFactory seedFactory;
+	private CustomerInventory customerInventory;
+	private CustomerTransactionParameters transactionParameters;
+	
+	public TransactionTimeSamplerBuilder(SeedFactory seedFactory)
+	{
+		this.seedFactory = seedFactory;
+	}
+	
+	public void setCustomerInventory(CustomerInventory inventory)
+	{
+		this.customerInventory = inventory;
+	}
+	
+	public void setCustomerTransactionParameters(CustomerTransactionParameters parameters)
+	{
+		this.transactionParameters = parameters;
+	}
+	
+	public TransactionTimeSampler build()
+	{
+		return new TransactionTimeSampler(transactionParameters.averageTransactionTriggerTime,
+				customerInventory, seedFactory);
+	}
+}
