a2703476ad0d859ba921bfe18670780afc1df6ba
RJ Nowling <rnowling@gmail.com>
2014-10-13 14:54:13 -0400

    Remove file reader dependencies in TestStoreStampler

--- src/java/bps-data-generator/src/test/java/com/github/rnowling/bps/datagenerator/generators/store/TestStoreSampler.java
+++ src/java/bps-data-generator/src/test/java/com/github/rnowling/bps/datagenerator/generators/store/TestStoreSampler.java
@@ -3,21 +3,18 @@ package com.github.rnowling.bps.datagenerator.generators.store;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-import java.util.List;
-import java.util.Map;
+import java.util.Arrays;
+import java.util.Collection;
 
 import org.junit.Test;
 
-import com.github.rnowling.bps.datagenerator.Constants;
+import com.github.rnowling.bps.datagenerator.datamodels.Pair;
 import com.github.rnowling.bps.datagenerator.datamodels.inputs.ZipcodeRecord;
 import com.github.rnowling.bps.datagenerator.datamodels.outputs.Store;
-import com.github.rnowling.bps.datagenerator.datareaders.ZipcodeReader;
 import com.github.rnowling.bps.datagenerator.framework.SeedFactory;
 import com.github.rnowling.bps.datagenerator.framework.samplers.RouletteWheelSampler;
 import com.github.rnowling.bps.datagenerator.framework.samplers.Sampler;
 import com.github.rnowling.bps.datagenerator.framework.samplers.SequenceSampler;
-import com.github.rnowling.bps.datagenerator.generators.store.StoreSampler;
-import com.google.common.collect.Maps;
 
 public class TestStoreSampler
 {
@@ -25,25 +22,16 @@ public class TestStoreSampler
 	@Test
 	public void testSampler() throws Exception
 	{
-		ZipcodeReader reader = new ZipcodeReader();
-		reader.setCoordinatesFile(Constants.COORDINATES_FILE);
-		reader.setIncomesFile(Constants.INCOMES_FILE);
-		reader.setPopulationFile(Constants.POPULATION_FILE);
-		
-		List<ZipcodeRecord> zipcodes = reader.readData();
-		
-		assertTrue(zipcodes.size() > 0);
+		Collection<ZipcodeRecord> zipcodes = Arrays.asList(new ZipcodeRecord[] {				
+				new ZipcodeRecord("11111", Pair.create(1.0, 1.0), 30000.0, 100),
+				new ZipcodeRecord("22222", Pair.create(2.0, 2.0), 45000.0, 200),
+				new ZipcodeRecord("33333", Pair.create(3.0, 3.0), 60000.0, 300)
+				});
 		
 		SeedFactory factory = new SeedFactory(1234);
 		
-		Map<ZipcodeRecord, Double> zipcodePDF = Maps.newHashMap();
-		for(ZipcodeRecord record : zipcodes)
-		{
-			zipcodePDF.put(record, 1.0);
-		}
-		
 		Sampler<Store> sampler = new StoreSampler(new SequenceSampler(), 
-				RouletteWheelSampler.create(zipcodePDF, factory));
+				RouletteWheelSampler.createUniform(zipcodes, factory));
 		
 		Store store = sampler.sample();
 		assertNotNull(store);
