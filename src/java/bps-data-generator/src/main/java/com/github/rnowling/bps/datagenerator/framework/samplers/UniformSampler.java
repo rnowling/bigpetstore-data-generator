69263da041d6c1144e01051180d1040d2056f7fa
RJ Nowling <rnowling@gmail.com>
2014-10-15 11:25:41 -0400

    Added ranges to uniform sampler

--- src/java/bps-data-generator/src/main/java/com/github/rnowling/bps/datagenerator/framework/samplers/UniformSampler.java
+++ src/java/bps-data-generator/src/main/java/com/github/rnowling/bps/datagenerator/framework/samplers/UniformSampler.java
@@ -7,14 +7,25 @@ import com.github.rnowling.bps.datagenerator.framework.SeedFactory;
 public class UniformSampler implements Sampler<Double>
 {
 	final Random rng;
+	final double lowerbound;
+	final double upperbound;
 	
 	public UniformSampler(SeedFactory seedFactory)
 	{
 		rng = new Random(seedFactory.getNextSeed());
+		lowerbound = 0.0;
+		upperbound = 1.0;
+	}
+	
+	public UniformSampler(double lowerbound, double upperbound, SeedFactory seedFactory)
+	{
+		rng = new Random(seedFactory.getNextSeed());
+		this.lowerbound = lowerbound;
+		this.upperbound = upperbound;
 	}
 	
 	public Double sample()
 	{
-		return rng.nextDouble();
+		return (upperbound - lowerbound) * rng.nextDouble() + lowerbound;
 	}
 }
