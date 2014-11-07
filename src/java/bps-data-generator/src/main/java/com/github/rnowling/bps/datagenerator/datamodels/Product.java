076322b5394874817b756702c34858bb1b2f28c1
RJ Nowling <rnowling@gmail.com>
2014-11-07 16:22:56 -0500

    Collapse datamodels a bit

---
+++ src/java/bps-data-generator/src/main/java/com/github/rnowling/bps/datagenerator/datamodels/Product.java
@@ -0,0 +1,79 @@
+package com.github.rnowling.bps.datagenerator.datamodels;
+
+import java.io.Serializable;
+import java.util.Map;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+
+public class Product implements Serializable
+{
+	ImmutableMap<String, Object> fields;
+	
+	public Product(Map<String, Object> fields)
+	{
+		this.fields = ImmutableMap.copyOf(fields);
+	}
+	
+	public ImmutableSet<String> getFieldNames()
+	{
+		return fields.keySet();
+	}
+	
+	public Object getFieldValue(String fieldName)
+	{
+		return fields.get(fieldName);
+	}
+	
+	public String getFieldValueAsString(String fieldName)
+	{
+		return fields.get(fieldName).toString();
+	}
+	
+	public Double getFieldValueAsDouble(String fieldName)
+	{
+		Object value = getFieldValue(fieldName);
+		try
+		{
+			Double doubleValue = (Double) value;
+			return doubleValue;
+		}
+		catch(ClassCastException e)
+		{
+			return null;
+		}
+	}
+	
+	public Long getFieldValueAsLong(String fieldName)
+	{
+		Object value = getFieldValue(fieldName);
+		try
+		{
+			Long longValue = (Long) value;
+			return longValue;
+		}
+		catch(ClassCastException e)
+		{
+			try
+			{
+				Integer intValue = (Integer) value;
+				return new Long(intValue);
+			}
+			catch(ClassCastException f)
+			{
+				return null;
+			}
+		}
+	}
+	
+	public String toString()
+	{
+		String str = "";
+		for(Map.Entry<String, Object> entry : fields.entrySet())
+		{
+			str += entry.getKey() + "=" + entry.getValue() + ";";
+		}
+		
+		return str;
+	}
+}
