cf3298d06eab1de175becf2f99dca5fbd6c24543
RJ Nowling <rnowling@gmail.com>
2014-11-07 11:50:25 -0500

    Modify tests to remove reader dependencies

--- src/java/bps-data-generator/src/test/java/com/github/rnowling/bps/datagenerator/generators/customer/TestCustomerSamplerBuilder.java
+++ src/java/bps-data-generator/src/test/java/com/github/rnowling/bps/datagenerator/generators/customer/TestCustomerSamplerBuilder.java
@@ -3,6 +3,7 @@ package com.github.rnowling.bps.datagenerator.generators.customer;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -12,6 +13,7 @@ import org.junit.Test;
 import com.github.rnowling.bps.datagenerator.datamodels.Pair;
 import com.github.rnowling.bps.datagenerator.datamodels.inputs.InputData;
 import com.github.rnowling.bps.datagenerator.datamodels.inputs.Names;
+import com.github.rnowling.bps.datagenerator.datamodels.inputs.ProductCategory;
 import com.github.rnowling.bps.datagenerator.datamodels.inputs.ZipcodeRecord;
 import com.github.rnowling.bps.datagenerator.datamodels.outputs.Customer;
 import com.github.rnowling.bps.datagenerator.datamodels.outputs.Store;
@@ -34,7 +36,8 @@ public class TestCustomerSamplerBuilder
 		
 		Names names = new Names(nameList, nameList);
 		
-		InputData inputData = new InputData(zipcodes, names);
+		// don't need product categories for building customers
+		InputData inputData = new InputData(zipcodes, names, new ArrayList<ProductCategory>());
 		
 		List<Store> stores = Arrays.asList(new Store(0, "Store_0", zipcodes.get(0)),
 				new Store(1, "Store_1", zipcodes.get(1)),
--- src/java/bps-data-generator/src/test/java/com/github/rnowling/bps/datagenerator/generators/purchasingprofile/TestPurchasingProfileSamplerBuilder.java
+++ src/java/bps-data-generator/src/test/java/com/github/rnowling/bps/datagenerator/generators/purchasingprofile/TestPurchasingProfileSamplerBuilder.java
@@ -8,23 +8,65 @@ import java.util.List;
 import org.junit.Test;
 
 import com.github.rnowling.bps.datagenerator.Constants;
+import com.github.rnowling.bps.datagenerator.datamodels.PetSpecies;
 import com.github.rnowling.bps.datagenerator.datamodels.inputs.ProductCategory;
-import com.github.rnowling.bps.datagenerator.datareaders.ProductsReader;
+import com.github.rnowling.bps.datagenerator.datamodels.inputs.ProductCategoryBuilder;
+import com.github.rnowling.bps.datagenerator.datamodels.simulation.Product;
 import com.github.rnowling.bps.datagenerator.framework.SeedFactory;
 import com.github.rnowling.bps.datagenerator.framework.samplers.Sampler;
-import com.github.rnowling.bps.datagenerator.generators.purchasingprofile.PurchasingProfileSamplerBuilder;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
 
 public class TestPurchasingProfileSamplerBuilder
 {
+	
+	private List<ProductCategory> createProducts()
+	{
+		List<ProductCategory> productCategories = Lists.newArrayList();
+		
+		ProductCategoryBuilder foodBuilder = new ProductCategoryBuilder();
+		foodBuilder.addApplicableSpecies(PetSpecies.DOG);
+		foodBuilder.setAmountUsedPetPetAverage(1.0);
+		foodBuilder.setAmountUsedPetPetVariance(1.0);
+		foodBuilder.setDailyUsageRate(2.0);
+		foodBuilder.addFieldName(Constants.PRODUCT_CATEGORY);
+		foodBuilder.addFieldName(Constants.PRODUCT_QUANTITY);
+		foodBuilder.addFieldName("Flavor");
+		foodBuilder.setCategory("dogFood");
+		foodBuilder.addProduct(new Product(ImmutableMap.of(Constants.PRODUCT_CATEGORY, (Object) "dogFood",
+				Constants.PRODUCT_QUANTITY, (Object) 60.0, "Flavor", "Fish & Potato")));
+		foodBuilder.addProduct(new Product(ImmutableMap.of(Constants.PRODUCT_CATEGORY, (Object) "dogFood",
+				Constants.PRODUCT_QUANTITY, (Object) 30.0, "Flavor", "Chicken & Rice")));
+		foodBuilder.addProduct(new Product(ImmutableMap.of(Constants.PRODUCT_CATEGORY, (Object) "dogFood",
+				Constants.PRODUCT_QUANTITY, (Object) 15.0, "Flavor", "Lamb & Barley")));
+		productCategories.add(foodBuilder.build());
+		
+		ProductCategoryBuilder bagBuilder = new ProductCategoryBuilder();
+		bagBuilder.addApplicableSpecies(PetSpecies.DOG);
+		bagBuilder.setAmountUsedPetPetAverage(1.0);
+		bagBuilder.setAmountUsedPetPetVariance(1.0);
+		bagBuilder.setDailyUsageRate(2.0);
+		bagBuilder.addFieldName(Constants.PRODUCT_CATEGORY);
+		bagBuilder.addFieldName(Constants.PRODUCT_QUANTITY);
+		bagBuilder.addFieldName("Color");
+		bagBuilder.setCategory("Poop Bags");
+		bagBuilder.addProduct(new Product(ImmutableMap.of(Constants.PRODUCT_CATEGORY, (Object) "Poop Bags",
+				Constants.PRODUCT_QUANTITY, (Object) 60.0, "Color", "Blue")));
+		bagBuilder.addProduct(new Product(ImmutableMap.of(Constants.PRODUCT_CATEGORY, (Object) "Poop Bags",
+				Constants.PRODUCT_QUANTITY, (Object) 30.0, "Color", "Red")));
+		bagBuilder.addProduct(new Product(ImmutableMap.of(Constants.PRODUCT_CATEGORY, (Object) "Poop Bags",
+				Constants.PRODUCT_QUANTITY, (Object) 120.0, "Color", "Multicolor")));
+		productCategories.add(bagBuilder.build());
+		
+		return productCategories;
+	}
 
 	@Test
 	public void testBuild() throws Exception
 	{
 		SeedFactory seedFactory = new SeedFactory(1245);
 		
-		ProductsReader reader = new ProductsReader(Constants.PRODUCTS_FILE);
-		
-		List<ProductCategory> productCategories = reader.readData();
+		List<ProductCategory> productCategories = createProducts();
 		
 		PurchasingProfileSamplerBuilder builder = new PurchasingProfileSamplerBuilder(productCategories, seedFactory);
 		Sampler<PurchasingProfile> sampler = builder.build();
